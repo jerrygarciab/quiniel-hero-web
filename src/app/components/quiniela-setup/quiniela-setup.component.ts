a231d51c01e94cb8cacf2ff6b1daef32371ea477
Emmanuel Benavides <kluzter@NemBook.local>
2018-01-04 07:02:06 -0600

    html de creacion de quiniela. sin errores de compilacion

--- src/app/components/quiniela-setup/quiniela-setup.component.ts
+++ src/app/components/quiniela-setup/quiniela-setup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-quiniela-setup',
@@ -6,10 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quiniela-setup.component.scss']
 })
 export class QuinielaSetupComponent implements OnInit {
+  public qsetupForm:    FormGroup;
+  public invalidField: boolean;
+  public quiniela:         string;
 
-  constructor() { }
+  constructor(private fb:        FormBuilder) {
+
+    this.createForm();
+
+    this.invalidField     = false;
+  }
 
   ngOnInit() {
   }
+  saveQuinielaInformation(post): void {
 
+    if (this.qsetupForm.valid) {
+
+      //this._firebase.setUser(post);
+      console.log(post);
+      this.qsetupForm.reset();
+
+    } else {
+
+      this.invalidField = true;
+
+    }
+
+  }
+  createForm(): void {
+    this.qsetupForm = this.fb.group({
+      quiniela:     [null, Validators.required]
+    })
+  }
 }
