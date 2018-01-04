a6c0e33c755888a8f0d284faec274e16e6c4c7bc
Emmanuel Benavides <kluzter@NemBook.local>
2018-01-04 05:43:49 -0600

    boton deshabilitado y desplegado de usuarios (hardlocked) no errors

--- src/app/components/invite-friends/invite-friends.component.ts
+++ src/app/components/invite-friends/invite-friends.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { ActivatedRoute }                                  from '@angular/router';
+import { FirebaseService }                                 from '../../shared/firebase.service';
+
+import { IUser }                                           from '../../shared/firebase';
 
 @Component({
   selector: 'app-invite-friends',
@@ -6,10 +11,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./invite-friends.component.scss']
 })
 export class InviteFriendsComponent implements OnInit {
+  
+  public usermap: Array<any>;
+  public invalidField: boolean;
+  public inviteForm:    FormGroup;
+  public theresalist: boolean = false;
 
-  constructor() { }
+  constructor(private fb:        FormBuilder,
+              ) {
+      this.createForm();
+      this.invalidField     = false;
+    }
 
   ngOnInit() {
+  }
+
+  searchFriends(): void {
+    this.usermap = [
+      { "userid":"ger10a", "nombre":"Gerardo", "username":"ger10" },
+      { "userid":"kluzter5", "nombre":"Emmanuel", "username":"kluzter" },
+      { "userid":"shizza7", "nombre":"Cesar", "username":"tolegol" }
+    ];
+    this.theresalist = true;
+  }
+  searchFriendsInformation(post): void {
+  
+        if (this.inviteForm.valid) {
+    
+          //this._firebase.setUser(post);
+          console.log(post);
+          this.inviteForm.reset();
+          
+        } else {
+    
+          this.invalidField = true;
+    
+        }
+    
+      }
+
+  
+  // Auxiliar Functions //
+
+  createForm(): void {
+    this.inviteForm = this.fb.group({
+      name:     [null, Validators.required],
+      username:     [null, Validators.required],
+      userID:   ['jdsjjjsdjdjsd'],
+    })
   }
 
 }
